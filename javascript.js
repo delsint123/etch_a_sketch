cefe748ab37ed553ac35b86955d96b9df5096ce4
delsint123 <delsint123@gmail.com>
2022-07-26 18:19:45 -0400

    Fix bugs and add reset button to menu

--- javascript.js
+++ javascript.js
@@ -1,20 +1,33 @@
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector('#canvas');
 
 for(let i = 0; i < 256; i++) { //256 will change to variable
     const squares = document.createElement('div');
-    squares.classList.add('square');
+    squares.classList.add('pixel');
     squares.setAttribute('id', i);
     document.getElementById('canvas').appendChild(squares);
 }
 
-const squares = document.querySelectorAll('.square');
-console.log(squares);
+const pixels = document.querySelectorAll('.pixel');
+console.log(pixels);
 
-squares.forEach(turnBlack);
+pixels.forEach(pixel => {
+    pixel.addEventListener('mouseover', (e) => {
+        const pixel = document.getElementById(e.composedPath()[0].id);
+        pixel.classList.add('black');
+    })
+});
 
-function turnBlack() {
-    addEventListener('mouseover', (e) => {
-        const square = document.getElementById(e.composedPath()[0].id);
-        square.classList.add('black');
+const menu = document.querySelector('#menu');
+
+const resetButton = document.createElement('button');
+resetButton.setAttribute('id','reset');
+resetButton.textContent = "Reset"
+menu.appendChild(resetButton);
+
+document.getElementById("reset").onclick = reset;
+
+function reset() {
+    pixels.forEach(pixel => {
+        pixel.classList.remove('black');
     })
 }
