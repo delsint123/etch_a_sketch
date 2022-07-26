8441c2308da9465b7909063c2ac0ef66db5c369c
delsint123 <delsint123@gmail.com>
2022-07-26 17:44:35 -0400

    Add default black brush

--- javascript.js
+++ javascript.js
@@ -1,6 +1,20 @@
-const container = document.querySelector('canvas');
+const canvas = document.querySelector('canvas');
 
 for(let i = 0; i < 256; i++) { //256 will change to variable
     const squares = document.createElement('div');
+    squares.classList.add('square');
+    squares.setAttribute('id', i);
     document.getElementById('canvas').appendChild(squares);
+}
+
+const squares = document.querySelectorAll('.square');
+console.log(squares);
+
+squares.forEach(turnBlack);
+
+function turnBlack() {
+    addEventListener('mouseover', (e) => {
+        const square = document.getElementById(e.composedPath()[0].id);
+        square.classList.add('black');
+    })
 }
